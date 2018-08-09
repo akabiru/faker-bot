a122987cc24efb198d049f240f94875c6c9fa993
Austin Kabiru <makabby@gmail.com>
2018-08-09 18:33:56 +0300

    feat(search): Search through full Faker namespace 🎉

--- lib/fakerbot/bot.rb
+++ lib/fakerbot/bot.rb
@@ -10,17 +10,43 @@ module FakerBot
           klass < self
         end
       end
+
+      def self.my_singleton_methods
+        if superclass
+          (singleton_methods - superclass.singleton_methods)
+        else
+          singleton_methods
+        end
+      end
     end
 
     class << self
       def find(query)
-        faker_descendants.select { |faker| faker.match?(/#{query}/i) }
+        search_descendants_matching(query)
+        matching_descendants
       end
 
       private
 
+      def search_descendants_matching(query)
+        faker_descendants.each do |faker|
+          methods = faker.my_singleton_methods
+          matching = methods.select { |m| m.match?(/#{query}/i) }
+          save_matching(faker, matching)
+        end
+      end
+
+      def save_matching(descendant, matching)
+        return if matching.empty?
+        matching_descendants[descendant].concat(matching)
+      end
+
+      def matching_descendants
+        @matching_descendants ||= Hash.new { |h, k| h[k] = [] }
+      end
+
       def faker_descendants
-        @faker_descendants ||= Faker::Base.descendants.map(&:to_s)
+        @faker_descendants ||= Faker::Base.descendants
       end
     end
   end
