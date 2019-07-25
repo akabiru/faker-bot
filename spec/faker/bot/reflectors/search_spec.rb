2cc8bae819b08d5dfba48d09148b4067ee8bd28a
bpleslie <bradleypleslie@gmail.com>
2019-07-25 14:15:59 -0700

    Allow search to accept namespaces and partial matches

--- spec/faker/bot/reflectors/search_spec.rb
+++ spec/faker/bot/reflectors/search_spec.rb
@@ -4,25 +4,48 @@ require 'spec_helper'
 
 RSpec.describe Faker::Bot::Reflectors::Search do
   describe '#call' do
-    context 'when a match is found' do
+    let(:result) { described_class.new(query).call }
+
+    context 'when passing a full method name' do
+      let(:query) { 'first_name' }
+
       it 'it returns the list of matches' do
-        query = 'firstname'
-
-        reflector = described_class.new(query)
-        result = reflector.call
-
         expect(result[Faker::Name]).to include(:first_name)
         expect(result).to be_a(Hash)
       end
     end
 
+    context 'when passing a partial method name' do
+      let(:query) { 'fir' }
+
+      it 'returns the list of matches' do
+        expect(result[Faker::Name]).to include(:first_name)
+        expect(result).to be_a(Hash)
+      end
+    end
+
+    context 'when passing a full class name' do
+      let(:query) { 'Beer' }
+
+      it 'returns the list of matches' do
+        expect(result).to include(Faker::Beer)
+        expect(result).to be_a(Hash)
+      end
+    end
+
+    context 'when passing a partial class name' do
+      let(:query) { 'bee' }
+
+      it 'returns the list of matches' do
+        expect(result).to include(Faker::Beer)
+        expect(result).to be_a(Hash)
+      end
+    end
+
     context 'when no match is found' do
+      let(:query) { 'firstname'}
+
       it 'returns an empty hash' do
-        query = 'foobar'
-
-        reflector = described_class.new(query)
-        result = reflector.call
-
         expect(result).to be_empty
         expect(result).to be_a(Hash)
       end
